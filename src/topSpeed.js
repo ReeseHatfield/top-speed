2a57f9d7077a391bf09110473840e7080f7c0922
Ean Hatfield <hatfield.69@wright.edu>
2024-08-19 18:34:27 -0400

    refactor

--- src/topSpeed.js
+++ src/topSpeed.js
@@ -7,28 +7,41 @@ const getSpeedElem = () => {
 }
 
 const getSpeed = () => {
-    curSpeed = getSpeedElem().innerText
-    
-    return parseFloat(curSpeed)
+    const curSpeed = getSpeedElem().innerText
+
+    const speedNum = parseFloat(curSpeed)
+
+    return speedNum.toFixed(1)
 }
 
 const updateSpeed = () => {
     const curSpeed = getSpeed()
 
-    if (curSpeed > topSpeed) {
-        topSpeed = curSpeed
-
-        const topSpeedElem = document.getElementById("ui-top-speed-val")
-        if (topSpeedElem) {
-            topSpeedElem.innerText = topSpeed
-        } else {
-            getSpeedElem().insertAdjacentHTML(
-                "afterend",
-                `<span id="ui-top-speed-val" class="ui-stat-val">${topSpeed}</span>`
-            )
-        }
+    if (curSpeed < topSpeed) {
+        return
     }
+
+    topSpeed = curSpeed
+
+    const topSpeedElem = document.getElementById("ui-top-speed-val")
+    if (topSpeedElem) {
+        topSpeedElem.innerText = topSpeed
+        return
+    }
+    
 }
+
+const createTopSpeedElem = () => {
+    const speedElem = getSpeedElem();
+    const topSpeedElem = document.createElement("span");
+
+    topSpeedElem.id = "ui-top-speed-val";
+    topSpeedElem.className = "ui-stat-val";
+    topSpeedElem.innerText = topSpeed;
+    speedElem.after(topSpeedElem);
+};
+
 const tickSpeed = 100
 
+createTopSpeedElem()
 setInterval(updateSpeed, tickSpeed)
